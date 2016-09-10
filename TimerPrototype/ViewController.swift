cd889e3ad26912bc85d53b58152817dc93989c39
utwang <x2x2tone@gmail.com>
2016-09-10 17:10:43 +0900

    collision square & barrier

--- TimerPrototype/ViewController.swift
+++ TimerPrototype/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    let barrier = UIView(frame: CGRect(x: 0, y: 300, width: 130, height: 20))
+
+    var animator: UIDynamicAnimator?
+    var gravity: UIGravityBehavior?
+    var collision: UICollisionBehavior?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        animator = UIDynamicAnimator(referenceView: view)
+        setSquare()
+        setBarrier()
+        setGravity()
+        setCollision()
+
+        collision?.action = {
+            print("\(NSStringFromCGAffineTransform(self.square.transform))\(NSStringFromCGPoint(self.square.center))")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func setSquare() {
+        square.backgroundColor = UIColor.gray
+        view.addSubview(square)
+    }
 
+
+    private func setBarrier() {
+        barrier.backgroundColor = UIColor.red
+        view.addSubview(barrier)
+    }
+
+    private func setGravity() {
+        gravity = UIGravityBehavior(items: [square])
+        guard let g = gravity else { return }
+        animator?.addBehavior(g)
+    }
+
+    private func setCollision() {
+//        collision = UICollisionBehavior(items: [square, barrier])
+        collision = UICollisionBehavior(items: [square])
+        collision?.addBoundary(withIdentifier: "barrier" as NSCopying, for: UIBezierPath(rect: barrier.frame))
+        collision?.translatesReferenceBoundsIntoBoundary = true
+        guard let col = collision else { return }
+        animator?.addBehavior(col)
+    }
 }
 
