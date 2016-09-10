baf2ec227d1efb696f512ca0eaf92646ecfcda77
utwang <x2x2tone@gmail.com>
2016-09-10 16:14:27 +0900

    initial commit

---
+++ TimerPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerPrototype
+//
+//  Created by Uchida Atsushi on 2016/09/10.
+//  Copyright © 2016年 Uchida Atsushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
